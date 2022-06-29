416cf650e0ea5d18bb83b0a00ea59ae2e456e729
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-29 10:58:54 +0200

    fields created + slugify module added

--- src/views/author/articles/index.vue
+++ src/views/author/articles/index.vue
@@ -1,39 +1,83 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
+
+      <el-form-item label="Title" required>
+        <el-input v-model="form.title" />
       </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
+
+      <el-form-item label="Categories" required>
+        <el-select
+          v-model="form.categoriesSelected"
+          style="display: block"
+          multiple
+          col="12"
+          filterable
+          default-first-option
+          :reserve-keyword="false"
+        >
+          <el-option
+            v-for="item in form.categories"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
       </el-form-item>
+
+      <el-form-item label="Content" required>
+        <el-input v-model="form.content" type="textarea" rows="10" />
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
       </el-form-item>
+
     </el-form>
   </div>
 </template>
 
 <script>
+
+import slugify from 'slugify'
+
 export default {
+
   data() {
     return {
       form: {
-        name: '',
-        desc: ''
+        userId: 'hidden',
+        slug: '',
+        title: '',
+        content: '',
+        categories: [
+          {
+            value: 'HTML',
+            label:
+              'Mon article sur la block chaine que je ne kiff pas trou mais woow que du bkabka'
+          },
+          {
+            value: 'CSS',
+            label: 'CSS'
+          },
+          {
+            value: 'JavaScript',
+            label: 'JavaScript'
+          }
+        ],
+        categoriesSelected: []
       }
     }
   },
   methods: {
     onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
+      for (const category of this.form.categoriesSelected) {
+        console.log(category)
+      }
+      this.form.slug = slugify(this.form.title, {
+        lower: true
       })
+      console.log(this.form.title)
     }
   }
 }
@@ -44,4 +88,3 @@ export default {
   text-align: center;
 }
 </style>
-
