465f78e52727a444d347de137dc8d58077694251
fakchichMohammed <43201814+fakchichMohammed@users.noreply.github.com>
2022-06-29 23:40:52 +0200

    mohammed : foprmated login view

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-    >
+    <el-form ref="loginForm" :model="loginForm" class="login-form" auto-complete="on" label-position="left">
       <div class="title-container">
         <h3 class="title">Login</h3>
       </div>
@@ -168,7 +162,7 @@ $cursor: #fff;
 
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
+        -webkit-text-fill-color: $cursor  !important;
       }
     }
   }
