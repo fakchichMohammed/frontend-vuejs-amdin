2d4a44dcb415515706e1f20b560c88099331d0e3
fakchichMohammed <43201814+fakchichMohammed@users.noreply.github.com>
2022-06-30 00:03:36 +0200

    mohammed : added delete, edit events to category api

--- src/api/categories.js
+++ src/api/categories.js
@@ -29,7 +29,77 @@ export function add(categoryInfo) {
       const { data } = response
 
       if (!data) {
-        return reject('Verification failed, please try again.')
+        return reject('Creation failed, please try again.')
+      }
+
+      // const { username, firstname } = data
+      console.log(data)
+      /* commit('SET_TOKEN', data.tokens.id_token)
+      setToken(data.tokens.id_token, data.tokens.expires_in)
+      commit('SET_NAME', username)
+      commit('SET_AVATAR', photo) */
+      // resolve()
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+
+// remove category
+export function remove(params) {
+  return request({
+    url: 'http://178.33.234.128:8022/api/category/',
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    params
+  })
+}
+
+// handle delete category
+export function deleteCategory(categoryInfo) {
+  const { title, description } = categoryInfo
+  return new Promise((resolve, reject) => {
+    remove({ title: title.trim(), description: description.trim() }).then(response => {
+      console.log('inside category then')
+      const { data } = response
+
+      if (!data) {
+        return reject('Delete failed, please try again.')
+      }
+
+      // const { username, firstname } = data
+      console.log(data)
+      /* commit('SET_TOKEN', data.tokens.id_token)
+      setToken(data.tokens.id_token, data.tokens.expires_in)
+      commit('SET_NAME', username)
+      commit('SET_AVATAR', photo) */
+      // resolve()
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+
+// edit category
+export function update(params) {
+  return request({
+    url: 'http://178.33.234.128:8022/api/category/',
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    params
+  })
+}
+
+// handle edit category
+export function edit(categoryInfo) {
+  const { title, description } = categoryInfo
+  return new Promise((resolve, reject) => {
+    update({ title: title.trim(), description: description.trim() }).then(response => {
+      console.log('inside category then')
+      const { data } = response
+
+      if (!data) {
+        return reject('Edit failed, please try again.')
       }
 
       // const { username, firstname } = data
