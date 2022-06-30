e8f1612cc7d580beaa624067af9611c216a94713
Amyrkhan B <beljattou amir>
2022-06-30 08:16:17 +0200

    Amir: adding firstname getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   photo: state => state.user.photo,
-  username: state => state.user.username
+  username: state => state.user.username,
+  first_name: state => state.user.first_name
 }
 export default getters
