99fead5d2f3d072af0a6f524b4452dda8adf403e
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-30 13:31:51 +0200

    create request to post an article

--- src/api/articles.js
+++ src/api/articles.js
@@ -1,20 +1,30 @@
 import request from '@/utils/request'
 import store from '@/store/index'
 
+const apiUrl = 'http://178.33.234.128:8022/api/articles/'
+
 export function getAllArticles() {
   return request({
-    url: 'http://178.33.234.128:8022/api/articles/',
+    url: apiUrl,
     method: 'get',
     headers: { 'Content-Type': 'application/json' }
   })
 }
 export function getMyArticles() {
   return request({
-    url: 'http://178.33.234.128:8022/api/articles/',
+    url: apiUrl,
     method: 'get',
     headers: { 'Content-Type': 'application/json' },
     params: {
       user_id: store.state.user.userId
     }
+  })
+}
+export function createArticle(data) {
+  return request({
+    url: apiUrl,
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    data
   })
 }
--- src/views/author/articles/addForm.vue
+++ src/views/author/articles/addForm.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
+    <el-form ref="form" :model="form" :rules="ArticleRules" label-width="120px">
 
-      <el-form-item label="Title" required>
-        <el-input v-model="form.title" />
+      <el-form-item label="Title" prop="title">
+        <el-input ref="title" v-model="form.title" />
       </el-form-item>
 
-      <el-form-item label="Categories" required>
+      <el-form-item label="Categories" prop="categoriesSelected">
         <el-select
+          ref="categoriesSelected"
           v-model="form.categoriesSelected"
           style="display: block"
           multiple
@@ -17,16 +18,16 @@
           :reserve-keyword="false"
         >
           <el-option
-            v-for="item in form.categories"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
+            v-for="item in categories"
+            :key="item.id"
+            :label="item.title"
+            :value="item.id"
           />
         </el-select>
       </el-form-item>
 
-      <el-form-item label="Content" required>
-        <el-input v-model="form.content" type="textarea" rows="10" />
+      <el-form-item label="Content" prop="content">
+        <el-input ref="content" v-model="form.content" type="textarea" rows="10" />
       </el-form-item>
 
       <el-form-item>
@@ -40,44 +41,76 @@
 <script>
 
 import slugify from 'slugify'
+import { getList } from '@/api/categories'
+import { createArticle } from '@/api/articles'
+import store from '@/store'
 
 export default {
 
   data() {
     return {
+      listLoading: true,
+      categories: [],
       form: {
         userId: 'hidden',
         slug: '',
         title: '',
         content: '',
-        categories: [
-          {
-            value: 'HTML',
-            label:
-              'Mon article sur la block chaine que je ne kiff pas trou mais woow que du bkabka'
-          },
-          {
-            value: 'CSS',
-            label: 'CSS'
-          },
-          {
-            value: 'JavaScript',
-            label: 'JavaScript'
-          }
-        ],
         categoriesSelected: []
+      },
+      ArticleRules: {
+        title: [
+          { required: true, trigger: 'blur' }
+        ],
+        content: [
+          { required: true, trigger: 'blur' }
+        ],
+        categoriesSelected: [
+          { required: true, trigger: 'blur' }
+        ]
       }
     }
   },
+  mounted() {
+    this.getCategories()
+  },
   methods: {
-    onSubmit() {
-      for (const category of this.form.categoriesSelected) {
-        console.log(category)
-      }
-      this.form.slug = slugify(this.form.title, {
-        lower: true
+    getCategories() {
+      this.listLoading = true
+      getList().then((response) => {
+        this.categories = response.data
+        this.listLoading = false
       })
-      console.log(this.form.title)
+    },
+    onSubmit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.loading = true
+          this.form.slug = slugify(this.form.title, { lower: true })
+          const categoriesToSubmit = []
+          for (const category in this.form.categoriesSelected) {
+            categoriesToSubmit.push(parseInt(category))
+          }
+          try {
+            createArticle({
+              author: store.userId,
+              title: this.form.title,
+              slug: this.form.slug,
+              content: this.form.content,
+              category: categoriesToSubmit
+            }).then(response => console.log(response))
+            this.loading = false
+            this.$message('Category created successfully!')
+          } catch (error) {
+            this.loading = false
+            this.$message('Category creation failed !')
+            console.log(error.message)
+          }
+        } else {
+          console.log('Category error!!')
+          return false
+        }
+      })
     }
   }
 }
